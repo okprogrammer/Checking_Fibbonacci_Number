c946ea61f5fc6724baec91052115864f3880485b
okprogrammmer <omk7912@gmail.com>
2018-04-05 15:47:27 +0530

    adding all basic stuffs

--- main.cpp
+++ main.cpp
@@ -1,47 +1,40 @@
-#include<iostream>
-
+#include <iostream>
+#include<vector>
+/* 0 -4 -6 0 -7 -6
+-1 -2 -6 -8 -3 -1
+-8 -4 -2 -8 -8 -6
+-3 -1 -2 -5 -7 -4
+-3 -5 -3 -6 -6 -6
+-3 -6 0 -8 -6 -7 */
 using namespace std;
 
-bool checkMember(int n){
 
-  /* Don't write main().
-   * Don't read input, it is passed as function argument.
-   * Return output and don't print it.
-   * Taking input and printing output is handled automatically.
-  */
-  int a[n+1],cot=0,i;
-  for(i=1;i<=n;i++)
-  {
-  if(i==1){
-    a[1]=1;
-  }
-  else if(i==2)
-  {
-    a[2]=1;
-  }
-  else
-    a[i]=a[i-1]+a[i-2];
+int main(){
+    int b[4][4]={{0}};
+    vector< vector<int> > arr(6,vector<int>(6));
+    for(int arr_i = 0;arr_i < 6;arr_i++){
+       for(int arr_j = 0;arr_j < 6;arr_j++){
+          cin >> arr[arr_i][arr_j];
+       }
+    }
 
-  }
-  for(int i=1;i<=n;i++) {
-    if(a[i]==n)
-      cot++;
-  }
-  if(cot!=0)
-  {
-    return true;
-  }
-  if(cot==0)
-  {
-    return false;
-  }
-
-}
-int main()
+    for(int i=0;i<4;i++)
 {
-    int n;
-    cout<<"Enter the number"<<endl;
-    cin>>n;
-    cout<<(bool)checkMember(n)<<endl;
+    for(int j=0;j<4;j++)
+    {
+        b[i][j]=arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
+    }
+}
+
+for(int i=0;i<4;i++)
+{
+    for(int j=0;j<4;j++)
+    {
+        if(b[i][j]>b[0][0])
+            b[0][0]=b[i][j];
+
+    }
+}
+cout<<b[0][0]<<endl;
     return 0;
 }
